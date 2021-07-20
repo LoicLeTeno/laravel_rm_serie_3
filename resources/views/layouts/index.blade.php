3a8cd018afb06e7a2db4b5a6bcd045086005b66e
le Téno <leteno.loic@gmail.com>
2021-07-20 12:00:25 +0200

    fin placement LLT

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,15 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Level HTML Template</title>
+    <!--
+
+Template 2095 Level
+
+http://www.tooplate.com/view/2095-level
+
+-->
+    <!-- load stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+    <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="{{ asset('font-awesome-4.7.0/css/font-awesome.min.css') }}"> <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"> <!-- Bootstrap style -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('slick/slick.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('slick/slick-theme.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/datepicker.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/tooplate-style.css') }}"> <!-- Templatemo style -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Série 3</title>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
+
 <body>
+    <div class="tm-main-content" id="top">
+        @include('partials.header')
+
+        @yield('content')
+
+        @include('partials.footer')
+    </div>
+
+    <!-- load JS files -->
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <!-- jQuery (https://jquery.com/download/) -->
+    <script src="js/popper.min.js"></script>
+    <!-- https://popper.js.org/ -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- https://getbootstrap.com/ -->
+    <script src="js/datepicker.min.js"></script>
+    <!-- https://github.com/qodesmith/datepicker -->
+    <script src="js/jquery.singlePageNav.min.js"></script>
+    <!-- Single Page Nav (https://github.com/ChrisWojcik/single-page-nav) -->
+    <script src="slick/slick.min.js"></script>
+    <!-- http://kenwheeler.github.io/slick/ -->
     
+    @include('partials.script.function')
 
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.index')
+
+@section('content')
+    @include('partials.sections.section-1')
+
+    @include('partials.sections.section-2')
+
+    @include('partials.sections.section-3')
+
+    @include('partials.sections.section-4')
+
+    @include('partials.sections.section-5-video')
+    
+    @include('partials.sections.section-6')
+@endsection
